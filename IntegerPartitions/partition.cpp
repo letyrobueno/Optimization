84e5939dabcfb5032917cf90528773eb82fd16d9
Letícia Rodrigues Bueno <leticia@madacenter.com>
2019-11-12 22:52:58 -0500

    Updating

--- IntegerPartitions/partition.cpp
+++ IntegerPartitions/partition.cpp
@@ -10,28 +10,28 @@
 using namespace std;
 
 void Partition(int m, int i, int k, string str){
-	string str2;
-	while (i<=k){
-		if ((m-i) >= i){
-			cout << m-i << str << " " << i << endl;
-    	  	if (((m-i)-i) >= i){
-				stringstream ss;
-				ss << i;
-				str2.append(str);
-				str2.append(" ");
-				str2 += ss.str();
-				Partition(m-i, i, k, str2);		
-			}
-    	  	i++;
-		} else i = k+1;
-	}
+    string str2;
+    while (i<=k){
+        if ((m-i) >= i){
+            cout << m-i << str << " " << i << endl;
+            if (((m-i)-i) >= i){
+                stringstream ss;
+                ss << i;
+                str2.append(str);
+                str2.append(" ");
+                str2 += ss.str();
+                Partition(m-i, i, k, str2);     
+            }
+            i++;
+        } else i = k+1;
+    }
 }
 
 main(){
-	int k, n;
-	string str;
-	cout << str << endl;
-	cout << "Enter with an integer: ";
+    int k, n;
+    string str;
+    cout << str << endl;
+    cout << "Enter with an integer: ";
     cin >> n;
     k = floor(n/2);
     Partition(n, 1, k, str);
